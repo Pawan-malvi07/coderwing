51dcdf661819028134804c1a2290d7f81489357f
trainee5 <165904484+trainee5@users.noreply.github.com>
2025-07-17 18:15:42 +0530

    new

--- controllers/Meeting.js
+++ controllers/Meeting.js
@@ -1,13 +1,26 @@
 const Event = require("../models/Meeting");
 
+
 const getAllEvents = async (req, res) => {
     try {
+        const now = new Date();
+
+        await Event.deleteMany({
+            $expr: {
+                $lt: [
+                    { $dateFromString: { dateString: { $concat: ["$date", "T", "$time"] } } },
+                    now
+                ]
+            }
+        });
+
         const events = await Event.find();
         res.json(events);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 };
+
 
 const addEvent = async (req, res) => {
     const { title, date, time } = req.body;
