be585217ab3656bad16756433216277ee35ea448
trainee5 <165904484+trainee5@users.noreply.github.com>
2025-07-25 16:42:34 +0530

    ok

--- controllers/courses-controller.js
+++ controllers/courses-controller.js
@@ -50,33 +50,38 @@ const Course = require("../models/courses");
  
 const addCourse = async (req, res) => {
   try {
-    const { title, description } = req.body;
- 
+    const { title, description, duration, technology, tags, courseDescription } = req.body;
+
     if (!title || !description) {
       return res.status(400).json({ msg: "Title and description are required!" });
     }
- 
+
     if (!req.file) {
       return res.status(400).json({ msg: "Image is required!" });
     }
- 
+
     const imageBase64 = req.file.buffer.toString("base64");
     const imageData = `data:${req.file.mimetype};base64,${imageBase64}`;
- 
+
     const newCourse = new Course({
       title,
       description,
       image: imageData,
+      duration: duration || "N/A",
+      technology: technology || "N/A",
+      tags: tags || "N/A",
+      courseDescription: courseDescription || "",
     });
- 
+
     await newCourse.save();
- 
+
     res.status(201).json({ msg: "Course added successfully!", course: newCourse });
   } catch (error) {
     console.error("Error adding course:", error);
     res.status(500).json({ msg: "Server error" });
   }
 };
+
  
 const getCourses = async (req, res) => {
   try {
